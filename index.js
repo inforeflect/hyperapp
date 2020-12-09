3ba36e91fa8bd9acd6f941c5861c48af9823abe5
zhuxp <nimezhu@gmail.com>
2020-12-09 13:06:59 -0500

    fix shouldRestart for subscriptions

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var createClass = (obj) => {
 
 var shouldRestart = (a, b) => {
   for (var k in { ...a, ...b }) {
-    if (typeof (isArray((b[k] = a[k])) ? b[k][0] : b[k]) === "function") {
+    if (typeof (isArray(b[k]) ? b[k][0] : b[k]) === "function") {
     } else if (a[k] !== b[k]) return true
   }
 }
